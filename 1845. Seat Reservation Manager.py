5b4781c7e5aea44c8373556e9ad45f9955ade6d1
akimov246 <akimov246@yandex.ru>
2023-11-06 22:47:35 +0700

    вариант решения задачи про кресла на двух массивах (работает еще медленнее, чем со словарем)

--- 1845. Seat Reservation Manager.py
+++ 1845. Seat Reservation Manager.py
@@ -1,34 +1,39 @@
-class SeatManager(object):
+import array
+
+class SeatManager:
+
     def __init__(self, n: int):
         self.n = self._is_valid_n(n)
-        self.seats = dict.fromkeys(range(1, self.n+1), False)
+        self.reserved_seats = []
+        self.unreserved_seats = list(range(1, self.n + 1))
 
-    def _is_valid_n(self, n):
+    def _is_valid_n(self, n: int) -> int:
         if n < 1:
-            raise ValueError("Сидений не может быть меньше одного")
+            raise ValueError
         return n
 
     def reserve(self) -> int:
-        if self.n == 1:
-            self.seats.update({1: True})
-            return 1
-        for seat in self.seats.keys():
-            if not self.seats.get(seat):
-                self.seats[seat] = True
-                return seat
+        seatNumber = min(self.unreserved_seats)
+        self.reserved_seats.append(seatNumber)
+        self.unreserved_seats.remove(seatNumber)
+        return seatNumber
 
     def unreserve(self, seatNumber: int) -> None:
-        if self.seats.get(seatNumber):
-            self.seats[seatNumber] = False
+        if seatNumber not in self.unreserved_seats:
+            self.unreserved_seats.append(seatNumber)
+            self.reserved_seats.remove(seatNumber)
 
-# Your SeatManager object will be instantiated and called as such:
-# obj = SeatManager(n)
-# param_1 = obj.reserve()
-# obj.unreserve(seatNumber)
 
-obj = SeatManager(5)
-print(obj.seats)
-obj.reserve()
-obj.reserve()
-obj.unreserve(1)
-print(obj.seats)
+import random
+import time
+
+N = 10000
+start = time.perf_counter()
+obj = SeatManager(N)
+for i in range(N):
+    r = random.randint(1, N)
+    action = ["obj.reserve()", f"obj.unreserve({r})"]
+    eval(random.choice(action))
+print(obj.reserved_seats)
+print(time.perf_counter() - start)
+

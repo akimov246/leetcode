c262fc5e16021cf92d2e67113dc68102a77a2654
akimov246 <akimov246@yandex.ru>
2023-11-07 00:11:26 +0700

    вариант с двумя numpy массивами. работает быстрее, но недостаточно

--- 1845. Seat Reservation Manager.py
+++ 1845. Seat Reservation Manager.py
@@ -1,11 +1,12 @@
-import array
+import numpy
 
 class SeatManager:
 
     def __init__(self, n: int):
         self.n = self._is_valid_n(n)
-        self.reserved_seats = []
-        self.unreserved_seats = list(range(1, self.n + 1))
+        self.reserved_seats = numpy.array([], dtype="uint32")
+        self.unreserved_seats = numpy.arange(1, self.n + 1, dtype="uint32")
+        print(self.unreserved_seats)
 
     def _is_valid_n(self, n: int) -> int:
         if n < 1:
@@ -13,26 +14,39 @@ class SeatManager:
         return n
 
     def reserve(self) -> int:
-        seatNumber = min(self.unreserved_seats)
-        self.reserved_seats.append(seatNumber)
-        self.unreserved_seats.remove(seatNumber)
-        return seatNumber
+        seatNumber = numpy.min(self.unreserved_seats)
+        index = numpy.where(self.unreserved_seats == seatNumber)
+        if index:
+            self.reserved_seats = numpy.append(self.reserved_seats, seatNumber)
+            self.unreserved_seats = numpy.delete(self.unreserved_seats, index)
+            return seatNumber
 
     def unreserve(self, seatNumber: int) -> None:
-        if seatNumber not in self.unreserved_seats:
-            self.unreserved_seats.append(seatNumber)
-            self.reserved_seats.remove(seatNumber)
+        index = numpy.where(self.unreserved_seats == seatNumber)
+        if index:
+            self.unreserved_seats = numpy.append(self.unreserved_seats, seatNumber)
+            index = numpy.where(self.reserved_seats == seatNumber)
+            self.reserved_seats = numpy.delete(self.reserved_seats, index)
 
 
 import random
 import time
 
-N = 10000
+# obj = SeatManager(5)
+# obj.reserve()
+# print(obj.reserved_seats)
+# obj.reserve()
+# print(obj.reserved_seats)
+# obj.unreserve(1)
+# print(obj.reserved_seats)
+
+N = 100000
 start = time.perf_counter()
 obj = SeatManager(N)
 for i in range(N):
     r = random.randint(1, N)
-    action = ["obj.reserve()", f"obj.unreserve({r})"]
+    #action = ["obj.reserve()", f"obj.unreserve({r})"]
+    action = ["obj.reserve()"]
     eval(random.choice(action))
 print(obj.reserved_seats)
 print(time.perf_counter() - start)

959af38549163aee5867929875a07d5901f59783
akimov246 <akimov246@yandex.ru>
2023-11-09 06:17:26 +0700

    valid parentheses v.1

--- 20. Valid Parentheses.py
+++ 20. Valid Parentheses.py
@@ -4,31 +4,45 @@ import random
 class Solution:
 
     DICT = {
-            "(": ")[{",
+            "(": "()[{",
             ")": "()[]{}",
-            "[": "]({",
+            "[": "[]({",
             "]": "()[]{}",
-            "{": "}([",
+            "{": "{}([",
             "}": "()[]{}",
             }
 
     def isValid(self, s: str) -> bool:
-        if (s.count("(") + s.count(")")) % 2 != 0:
-            return False
-        if (s.count("[") + s.count("]")) % 2 != 0:
-            return False
-        if (s.count("{") + s.count("}")) % 2 != 0:
-            return False
         if s[-1] in "([{":
             return False
-        if len(s) < 2:
+        if len(s) % 2 != 0:
             return False
         possible = "([{"
         for char in s:
             if char not in possible:
                 return False
             possible = self.DICT.get(char)
-        return True
+
+        s = list(s)
+        while True:
+            try:
+                if len(s) == 0:
+                    return True
+                char = s[0]
+                match char:
+                    case "("|")":
+                        s.remove("(")
+                        s.remove(")")
+                    case "["|"]":
+                        s.remove("[")
+                        s.remove("]")
+                    case "{"|"}":
+                        s.remove("{")
+                        s.remove("}")
+                    case _:
+                        return False
+            except:
+                return False
 
 
-print(Solution().isValid("()"))
+print(Solution().isValid("[([]])"))

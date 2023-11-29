10807fa83890993c55dcba17e6feca4098e21045
akimov246 <akimov246@yandex.ru>
2023-11-29 21:38:38 +0700

    257. Binary Tree Paths. Solved!

--- 257. Binary Tree Paths.py
+++ 257. Binary Tree Paths.py
@@ -13,11 +13,24 @@ class TreeNode:
 
 
 class Solution:
-    def __init__(self):
-        self.paths = []
-
     def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-        pass
+        self.res = []
+        path = ""
+        self.helper(root, path)
+        return self.res
+
+
+    def helper(self, root: TreeNode, path: str):
+        path += str(root.val)
+        if root.left is None and root.right is None:
+            self.res.append(path)
+        else:
+            path += "->"
+            if root.left:
+                self.helper(root.left, path)
+            if root.right:
+                self.helper(root.right, path)
+
 
 
 
@@ -27,8 +40,7 @@ tree = TreeNode(1)
 tree.left = TreeNode(2)
 tree.right = TreeNode(3)
 tree.left.right = TreeNode(5)
-tree.left.left = TreeNode(6)
-tree.left.left.right = TreeNode(7)
+# tree.left.left = TreeNode(6)
+# tree.left.left.right = TreeNode(7)
 
 print(Solution().binaryTreePaths(tree))
-print(Solution().paths)

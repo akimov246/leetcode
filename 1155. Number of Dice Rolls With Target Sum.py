530d069d5f046931dc423d312a9fee1c11f919d6
akimov246 <akimov246@yandex.ru>
2023-12-27 04:14:41 +0700

    1155. Number of Dice Rolls With Target Sum. In progress!

---
+++ 1155. Number of Dice Rolls With Target Sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numRollsToTarget(self, n: int, k: int, target: int) -> int:
+        for i in range(30 ** 30):
+            pass
+
+
+
+
+print(Solution().numRollsToTarget(2, 6, 7))
+
+#1 1 1 1 1 1 1
+#1 1 1 1 1 2
+#1 1 1 1 3
+#1 1 1 4
+#1 1 5
+#1 6
+#2 2 2 1
+#2 2 3
+#2 5
+#3 3 1
+#3 4
+#4 3
+#5 2
+#6 1
+#

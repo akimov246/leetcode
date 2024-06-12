c07c0a64250643177d82f2474844ad9c658437c9
akimov246 <akimov246@yandex.ru>
2024-06-12 07:14:08 +0700

    1974. Minimum Time to Type Word Using Special Typewriter. Solved!

--- 1974. Minimum Time to Type Word Using Special Typewriter.py
+++ 1974. Minimum Time to Type Word Using Special Typewriter.py
@@ -2,15 +2,19 @@ from string import ascii_lowercase
 
 class Solution:
     def minTimeToType(self, word: str) -> int:
-        alphabet = {letter:n for letter, n in zip(ascii_lowercase, range(len(ascii_lowercase)))}
-        print(alphabet)
         seconds = 0
         pointer = 'a'
         i = 0
         while i < len(word):
             if word[i] != pointer:
-                tmp = (alphabet[pointer] + alphabet[word[i]]) % len(alphabet)
-                seconds += (alphabet[pointer] + alphabet[word[i]]) % len(alphabet)
+                pntr_idx = ascii_lowercase.index(pointer)
+                clockwise = ascii_lowercase[pntr_idx:] + ascii_lowercase[:pntr_idx]
+                counterclockwise = clockwise[0] + clockwise[1:][::-1]
+                for shift, (cw, ccw) in enumerate(zip(clockwise, counterclockwise)):
+                    if word[i] in [cw, ccw]:
+                        seconds += shift
+                        break
+                #seconds += min(clockwise.index(word[i]), counterclockwise.index(word[i]))
                 pointer = word[i]
             else:
                 seconds += 1

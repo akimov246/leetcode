c7a5212e0eddf14138fe2bc5b585ba68d46c44d4
akimov246 <akimov246@yandex.ru>
2024-07-26 06:39:20 +0700

    912. Sort an Array. Still in Progress!

--- 912. Sort an Array.py
+++ 912. Sort an Array.py
@@ -1,3 +1,5 @@
+from typing import Iterable
+
 class TreeNode:
 
     def __init__(self, value):
@@ -17,13 +19,52 @@ class TreeNode:
     def __repr__(self):
         return f'Значение: {self.value}, количество: {self.quantity}'
 
+class Tree:
+
+    def __init__(self, nums: Iterable):
+        self.root = None
+        nums = tuple(nums)
+        self.__make_tree(nums)
+
+    def __make_tree(self, nums: Iterable):
+        for num in nums:
+            if self.root is None:
+                self.root = TreeNode(num)
+            else:
+                self.add_node(self.root, num)
+
+    def add_node(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = TreeNode(value)
+            else:
+                self.add_node(node.left, value)
+        elif value == node.value:
+            node.quantity += 1
+        else:
+            if node.right is None:
+                node.right = TreeNode(value)
+            else:
+                self.add_node(node.right, value)
+
+class Solution:
+    def sortArray(self, nums: list[int]) -> list[int]:
+        result = []
+        tree = Tree(nums)
+
+        def helper(node: TreeNode):
+            if node:
+                helper(node.left)
+                result.extend([node.value] * node.quantity)
+                helper(node.right)
+
+        helper(tree.root)
+        return result
+
+#print(Solution().sortArray(nums = list(range(1, 500 + 1))))
+
 class Solution:
     def sortArray(self, nums: list[int]) -> list[int]:
         pass
 
-
-tree = TreeNode(1)
-tree.quantity += 1
-print(tree)
-
-print(Solution().sortArray(nums = [5,2,3,1]))
+print(Solution().sortArray(nums = [5,1,1,2,0,0]))

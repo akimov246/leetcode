f4e70dbf69dd4e531532c0791718567833c758c6
akimov246 <akimov246@yandex.ru>
2024-07-29 16:48:20 +0700

    Same

--- 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.py
+++ 1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance.py
@@ -1,34 +1,29 @@
+from collections import defaultdict
+
 class Solution:
     def findTheCity(self, n: int, edges: list[list[int]], distanceThreshold: int) -> int:
+        self.edges = edges
+        self.neighbours = self.__neighbours()
+        print(self.neighbours)
 
-        reach = set()
-        visited = set()
-        def helper(city, distance = 0):
-            #print(distance)
-            neighbors = []
-            for f, t, d in edges:
-                if city == f:
-                    neighbors.append((t, d))
-                elif city == t:
-                    neighbors.append((f, d))
-            #print(neighbors)
-            for neighbor, d in neighbors:
-                if neighbor not in visited and distance + d <= distanceThreshold:
-                    reach.add(neighbor)
-                    helper(neighbor, distance + d)
-                    visited.add(city)
+        def helper(city, distance = 0, visited = None, reached = None):
+            if visited is None:
+                visited = set()
+            if reached is None:
+                reached = set()
+            for neighbour, dist in self.neighbours[city].items():
+                print(neighbour, dist)
 
+        helper(1)
 
-        result = {}
-        for city in range(n):
-            helper(city)
-            if city in reach:
-                reach.remove(city)
-            result[city] = reach
-            reach = set()
-            visited = set()
-
-        return sorted(result.items(), key=lambda item:(len(item[1]), -item[0]))[0][0]
+    def __neighbours(self):
+        result = defaultdict(dict)
+        for from_, to_, dist in self.edges:
+            result[from_].update({to_:dist})
+            result[to_].update({from_:dist})
+        return dict(result)
 
 
-print(Solution().findTheCity(n = 5, edges = [[0,1,2],[0,4,8],[1,2,3],[1,4,2],[2,3,1],[3,4,1]], distanceThreshold = 2))
+
+
+print(Solution().findTheCity(n = 4, edges = [[0,1,3],[1,2,1],[1,3,4],[2,3,1]], distanceThreshold = 4))

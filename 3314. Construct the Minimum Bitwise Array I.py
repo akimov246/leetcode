20cf34e4706daa0abf0c2509a187504bbcce2e7c
akimov246 <akimov246@yandex.ru>
2025-09-30 02:43:39 +0700

    3314. Construct the Minimum Bitwise Array I. Solved!

---
+++ 3314. Construct the Minimum Bitwise Array I.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minBitwiseArray(self, nums: list[int]) -> list[int]:
+        ans = []
+        for n in nums:
+            for m in range(n):
+                if m | (m + 1) == n:
+                    ans.append(m)
+                    break
+            else:
+                ans.append(-1)
+
+        return ans
+
+
+print(Solution().minBitwiseArray(nums = [22222222222222222222222222222222222]))

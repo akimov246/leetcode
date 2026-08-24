2ab5543c4bca44533d28855e925430bae418f8ab
Leonid Akimov <akimov246>
2026-08-24 16:19:16 +0700

    go:done: 14. Longest Common Prefix

---
+++ Go/14LongestCommonPrefix/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"strings"
+)
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+	fmt.Println(longestCommonPrefix([]string{"dog","racecar","car"}))
+}
+
+func longestCommonPrefix(strs []string) string {
+	currentLength := math.MaxInt
+	for _, word := range strs {
+		currentLength = min(len(word), currentLength)
+	}
+
+	outer:
+	for currentLength > 0 {
+		prefix := strs[0][:currentLength]
+		for _, word := range strs {
+			if !strings.HasPrefix(word, prefix) {
+				currentLength--
+				continue outer
+			} 
+		}
+		return prefix
+	}
+	return ""
+}

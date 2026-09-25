707d3c8fa7ec183627b9d36deac37f5156281569
Leonid Akimov <akimov246>
2026-09-25 23:13:53 +0700

    go:done: 67. Add Binary

--- Go/67AddBinary/main.go
+++ Go/67AddBinary/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"slices"
 )
 
 func main() {
@@ -10,6 +11,27 @@ func main() {
 }
 
 func addBinary(a string, b string) string {
-	carryTheOne := false
-	
+	var result []byte
+	carry := 0
+	i := len(a) - 1
+	j := len(b) - 1
+
+	for i >= 0 || j >= 0 || carry > 0 {
+		sum := 0
+		if i >= 0 {
+			sum += int(a[i])
+			i--
+		}
+		if j >= 0 {
+			sum += int(b[j])
+			j--
+		}
+		sum += carry
+
+		result = append(result, byte('0'+sum%2))
+		carry = sum / 2 % 2
+	}
+
+	slices.Reverse(result)
+	return string(result)
 }

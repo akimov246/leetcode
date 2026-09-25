e54a56ee51d97457ff9221c312c1b0d0f3999fa1
Leonid Akimov <akimov246>
2026-09-25 21:16:57 +0700

    go:wip: 67. Add Binary

---
+++ Go/67AddBinary/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(addBinary("11", "1"))
+	fmt.Println(addBinary("1010", "1011"))
+}
+
+func addBinary(a string, b string) string {
+	carryTheOne := false
+	
+}
